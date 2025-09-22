3016c4bb59f20a317ef26192e1e3e04f90621e71
amirkhosrovosughi <amirkhosro.vosughi@gmail.com>
2025-09-22 07:32:06 -0700

    [doxygen] - add doxygen documentation for package feature_2dto3d_transfer and modify some variable names inside the package

--- feature_2dto3d_transfer/include/feature_2dto3d_transfer/feature_2dto3d_transfer.hpp
+++ feature_2dto3d_transfer/include/feature_2dto3d_transfer/feature_2dto3d_transfer.hpp
@@ -15,6 +15,10 @@
 #include "common_utilities/transform_util.hpp"
 #include <px4_msgs/msg/vehicle_odometry.hpp>
 
+/**
+ * @class Feature2DTo3DTransfer
+ * @brief Node to convert 2D detected features into 3D coordinates using camera info and TF transforms.
+ */
 class Feature2DTo3DTransfer : public rclcpp::Node {
   typedef Eigen::Matrix<double, 3, 4> Matrix3x4;
   typedef Eigen::Matrix<double, 4, 3> Matrix4x3;
@@ -23,33 +27,49 @@ class Feature2DTo3DTransfer : public rclcpp::Node {
   typedef Eigen::Matrix<double, 4, 1> Vector4;
 
 public:
+  /**
+   * @brief Construct a new Feature2DTo3DTransfer node.
+   */
   Feature2DTo3DTransfer();
 
 private:
-  void coordinate2DCallback(const drone_msgs::msg::DetectedFeatureList cooerdinate2DList);
+  /**
+   * @brief Callback to process incoming 2D feature coordinates and convert them to 3D points.
+   * @param coordinate2DList List of detected 2D features with depth info.
+   */
+  void coordinate2DCallback(const drone_msgs::msg::DetectedFeatureList coordinate2DList);
+
+  /**
+   * @brief Callback to store camera intrinsic parameters from CameraInfo message.
+   * @param cameraInfo Camera intrinsic info.
+   */
   void cameraInfoCallback(const sensor_msgs::msg::CameraInfo cameraInfo);
+
+  /**
+   * @brief Update the transformation matrix between base and camera frames.
+   */
   void updateTransform();
 
 private:
-  rclcpp::Subscription<drone_msgs::msg::DetectedFeatureList>::SharedPtr _featureCoordinateSubscriber;
-  rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr _cameraInfoSubscriber;
+  rclcpp::Subscription<drone_msgs::msg::DetectedFeatureList>::SharedPtr _featureCoordinateSubscriber; ///< Subscriber for 2D feature coordinates
+  rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr _cameraInfoSubscriber; ///< Subscriber for camera intrinsic information
 
-  rclcpp::Publisher<drone_msgs::msg::PointList>::SharedPtr _feature3DcoordinateCameraPublisher;
-  rclcpp::Publisher<drone_msgs::msg::PointList>::SharedPtr _feature3DcoordinateBasePublisher;
+  rclcpp::Publisher<drone_msgs::msg::PointList>::SharedPtr _feature3DCoordinateCameraPublisher; ///< Publisher for 3D features in camera frame
+  rclcpp::Publisher<drone_msgs::msg::PointList>::SharedPtr _feature3DCoordinateBasePublisher; ///< Publisher for 3D features in base frame
 
-  std::unique_ptr<tf2_ros::Buffer> _tfBuffer;
-  std::shared_ptr<tf2_ros::TransformListener> _tflistener;
-  rclcpp::TimerBase::SharedPtr _timer;
+  std::unique_ptr<tf2_ros::Buffer> _tfBuffer; ///< TF2 buffer for transform lookup
+  std::shared_ptr<tf2_ros::TransformListener> _tfListener; ///< TF2 listener
+  rclcpp::TimerBase::SharedPtr _timer; ///< Timer for updating transforms
 
-  float _frameWidth;
-  float _frameHeight;
-  float _frameCx;
-  float _frameCy; 
-  float _frameFx;
-  float _frameFy;
-  Matrix4x4 _base2Camera;
-  bool _cameraInfoLoaded = false;
-  bool _cameraTransformLoaded = false;
+  float _frameWidth; ///< Camera frame width
+  float _frameHeight; ///< Camera frame height
+  float _frameCx; ///< Principal point cx
+  float _frameCy; ///< Principal point cy
+  float _frameFx; ///< Focal length fx
+  float _frameFy; ///< Focal length fy
+  Matrix4x4 _base2Camera; ///< Transform matrix from base to camera
+  bool _cameraInfoLoaded = false; ///< Flag indicating if camera info has been received
+  bool _cameraTransformLoaded = false; ///< Flag indicating if camera transform has been loaded
 };
 
 #endif  // FEATURE_2DTO3D_TRANSFER_HPP 
--- feature_2dto3d_transfer/src/feature_2dto3d_transfer.cpp
+++ feature_2dto3d_transfer/src/feature_2dto3d_transfer.cpp
@@ -1,14 +1,13 @@
 // feature_2dto3d_transfer.cpp
 
 #include "feature_2dto3d_transfer.hpp"
-#ifdef SOTRE_DEBUG_DATA
+#ifdef STORE_DEBUG_DATA
 #include "data_logging_utils/data_logger.hpp"
 #endif
 #include <cmath>
 
 const std::string FROM_FRAME = "base_link";
 const std::string TO_FRAME = "camera_frame";
-const rclcpp::Duration maxTimeDifferent = rclcpp::Duration(0,50000000);
 
 Feature2DTo3DTransfer::Feature2DTo3DTransfer()
   : Node("feature_2dto3d_transfer")
@@ -20,44 +19,44 @@ Feature2DTo3DTransfer::Feature2DTo3DTransfer()
 
   // need to subscribe to tf camera relative position from drone base
   _tfBuffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
-  _tflistener = std::make_shared<tf2_ros::TransformListener>(*_tfBuffer);
+  _tfListener = std::make_shared<tf2_ros::TransformListener>(*_tfBuffer);
 
   
   // create publisher
-  _feature3DcoordinateCameraPublisher = this->create_publisher<drone_msgs::msg::PointList>("/feature/coordinate/cameraFrame", 10);
-  _feature3DcoordinateBasePublisher = this->create_publisher<drone_msgs::msg::PointList>("/feature/coordinate/baseLink", 10);
+  _feature3DCoordinateCameraPublisher = this->create_publisher<drone_msgs::msg::PointList>("/feature/coordinate/cameraFrame", 10);
+  _feature3DCoordinateBasePublisher = this->create_publisher<drone_msgs::msg::PointList>("/feature/coordinate/baseLink", 10);
 
   //TODO: later change it in way that stops listening when it gets the valiue
   _timer = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&Feature2DTo3DTransfer::updateTransform, this));
 }
 
-void Feature2DTo3DTransfer::coordinate2DCallback(const drone_msgs::msg::DetectedFeatureList cooerdinate2DList)
+void Feature2DTo3DTransfer::coordinate2DCallback(const drone_msgs::msg::DetectedFeatureList coordinate2DList)
 {
-  #ifdef SOTRE_DEBUG_DATA
+  #ifdef STORE_DEBUG_DATA
   int plotCounter = 0;
   #endif
   if (_cameraInfoLoaded)
   {
-    if (cooerdinate2DList.features.size() != 0)
+    if (coordinate2DList.features.size() != 0)
     {
-      RCLCPP_DEBUG(this->get_logger(), "get %d feature 2D coordinate(s)", cooerdinate2DList.features.size());
+      RCLCPP_DEBUG(this->get_logger(), "get %d feature 2D coordinate(s)", coordinate2DList.features.size());
     }
     rclcpp::Time sampleTime = rclcpp::Clock().now();
 
     drone_msgs::msg::PointList pointListCamera;
     drone_msgs::msg::PointList pointListBase;
-    for (drone_msgs::msg::DetectedFeature cooerdinate2D : cooerdinate2DList.features)
+    for (drone_msgs::msg::DetectedFeature coordinate2D : coordinate2DList.features)
     {
-      #ifdef SOTRE_DEBUG_DATA
+      #ifdef STORE_DEBUG_DATA
       std::map<std::string, double> mapLog;  // add plotting
       #endif
-      RCLCPP_DEBUG(this->get_logger(), "2D coordinate: (x,y) = (%f,%f)", cooerdinate2D.x , cooerdinate2D.y);
+      RCLCPP_DEBUG(this->get_logger(), "2D coordinate: (x,y) = (%f,%f)", coordinate2D.x , coordinate2D.y);
       float estimatedDepth = 0.0;
 
-      float pixelX = (cooerdinate2D.x + 0.5) * _frameWidth;
-      float pixelY = (cooerdinate2D.y + 0.5) * _frameHeight;
+      float pixelX = (coordinate2D.x + 0.5) * _frameWidth;
+      float pixelY = (coordinate2D.y + 0.5) * _frameHeight;
 
-      #ifdef SOTRE_DEBUG_DATA
+      #ifdef STORE_DEBUG_DATA
       mapLog["pixelX_" + std::to_string(plotCounter)] = pixelX;
       mapLog["pixelY_" + std::to_string(plotCounter)] = pixelY;
       #endif
@@ -71,45 +70,45 @@ void Feature2DTo3DTransfer::coordinate2DCallback(const drone_msgs::msg::Detected
       Vector3 unitVector = pixelDirection; //.normalized(); no need to normalization here as depth camera return depth in z axis direction not  distance
       RCLCPP_DEBUG(this->get_logger(), " unitVector (%f,%f,%f)", unitVector[0], unitVector[1], unitVector[2]);
 
-      RCLCPP_DEBUG(this->get_logger(), "stereo camera depth is %f", cooerdinate2D.depth);
-      if (cooerdinate2D.depth <= 0.0 || cooerdinate2D.depth > 100.0)
+      RCLCPP_DEBUG(this->get_logger(), "stereo camera depth is %f", coordinate2D.depth);
+      if (coordinate2D.depth <= 0.0 || coordinate2D.depth > 100.0)
       {
         RCLCPP_INFO(this->get_logger(), "Skipped feature since depth value is not available");
         continue; //skip if depth data is not available
       }
       else
       {
-        estimatedDepth = cooerdinate2D.depth;
+        estimatedDepth = coordinate2D.depth;
         RCLCPP_DEBUG(this->get_logger(), "using camera depth %f", estimatedDepth);
       }
 
-      #ifdef SOTRE_DEBUG_DATA
-      mapLog["cooerdinate2D.depth_" + std::to_string(plotCounter)] = cooerdinate2D.depth;
+      #ifdef STORE_DEBUG_DATA
+      mapLog["coordinate2D.depth_" + std::to_string(plotCounter)] = coordinate2D.depth;
       #endif
     
-      Vector3 camera2featrue = unitVector * estimatedDepth;
+      Vector3 camera2Feature = unitVector * estimatedDepth;
 
       geometry_msgs::msg::Point point;
-      point.x = camera2featrue[0];
-      point.y = camera2featrue[1];
-      point.z = camera2featrue[2];
+      point.x = camera2Feature[0];
+      point.y = camera2Feature[1];
+      point.z = camera2Feature[2];
       pointListCamera.points.push_back(point);
 
-      #ifdef SOTRE_DEBUG_DATA
-      mapLog["camera2featrue[0]_" + std::to_string(plotCounter)] = camera2featrue[0];
-      mapLog["camera2featrue[1]_" + std::to_string(plotCounter)] = camera2featrue[1];
-      mapLog["camera2featrue[2]_" + std::to_string(plotCounter)] = camera2featrue[2];
+      #ifdef STORE_DEBUG_DATA
+      mapLog["camera2Feature[0]_" + std::to_string(plotCounter)] = camera2Feature[0];
+      mapLog["camera2Feature[1]_" + std::to_string(plotCounter)] = camera2Feature[1];
+      mapLog["camera2Feature[2]_" + std::to_string(plotCounter)] = camera2Feature[2];
       #endif
 
       if (_cameraTransformLoaded)
       {
-        Vector4 cameraFeatureHomogeneous = camera2featrue.homogeneous();
+        Vector4 cameraFeatureHomogeneous = camera2Feature.homogeneous();
         RCLCPP_DEBUG(this->get_logger(), " cameraFeatureHomogeneous(%f,%f,%f)", cameraFeatureHomogeneous[0], cameraFeatureHomogeneous[1], cameraFeatureHomogeneous[2]);
 
         Vector4 base2feature = _base2Camera*cameraFeatureHomogeneous;
         RCLCPP_DEBUG(this->get_logger(), " base2feature in NED (%f,%f,%f)", base2feature[0], base2feature[1], base2feature[2]);
 
-        #ifdef SOTRE_DEBUG_DATA
+        #ifdef STORE_DEBUG_DATA
         mapLog["base2feature[0]_" + std::to_string(plotCounter)] = base2feature[0];
         mapLog["base2feature[1]_" + std::to_string(plotCounter)] = base2feature[1];
         mapLog["base2feature[2]_" + std::to_string(plotCounter)] = base2feature[2];
@@ -122,17 +121,17 @@ void Feature2DTo3DTransfer::coordinate2DCallback(const drone_msgs::msg::Detected
         pointListBase.points.push_back(pointB);
       }
 
-      #ifdef SOTRE_DEBUG_DATA
+      #ifdef STORE_DEBUG_DATA
       data_logging_utils::DataLogger::log(mapLog);
       plotCounter++;
       #endif
     }
 
-    _feature3DcoordinateCameraPublisher->publish(pointListCamera);
+    _feature3DCoordinateCameraPublisher->publish(pointListCamera);
 
     if (_cameraTransformLoaded)
     {
-      _feature3DcoordinateBasePublisher->publish(pointListBase);
+      _feature3DCoordinateBasePublisher->publish(pointListBase);
     }
   }
 }
@@ -179,11 +178,11 @@ void Feature2DTo3DTransfer::updateTransform()
       Eigen::Matrix4d body_to_optical_transform = Eigen::Matrix4d::Identity();
       body_to_optical_transform.block<3, 3>(0, 0) << 0, 0, 1,
                                                     -1, 0, 0,
-                                                    0, -1, 0; // TODO: this is almost correct, just z is in wrong direction  
+                                                    0, -1, 0;
       _base2Camera =  _base2Camera * body_to_optical_transform;
       
       _cameraTransformLoaded = true;
-      RCLCPP_INFO(this->get_logger(), "Camera relative coordinate in loaded successfully");
+      RCLCPP_INFO(this->get_logger(), "Camera relative coordinate is loaded successfully");
   
     } catch (tf2::TransformException & ex) {
       RCLCPP_INFO(this->get_logger(), "Could not find transform %s to %s: %s", TO_FRAME.c_str(), FROM_FRAME.c_str(), ex.what());
